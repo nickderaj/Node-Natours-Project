ae475554d3d5dea6fca4edec2b66e383d931f41c
Nick De Raj <nickderaj@gmail.com>
2021-07-09 15:14:48 +0800

    Adding patch requests

--- app.js
+++ app.js
@@ -1,32 +1,91 @@
 const fs = require('fs');
 const http = require('http');
 const express = require('express');
-// express is a function, which upon calling will add a bunch of methods to our app like below:
 const app = express();
+app.use(express.json()); // middleware : function that can modify the incoming data between the request and response. The data from the body is added to the request object data using this middleware.
 
-// HTTP Method for the get request - testing functionality of Express
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app: 'Natours' });
-// }); // .json also sets the content-type to 'json' automatically, so we don't have to do it
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
-// });
-
-const toursJSON = JSON.parse(
+const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
-    results: toursJSON.length,
+    results: tours.length,
     data: {
-      tours: toursJSON,
+      tours: tours,
     },
   });
+});
+
+app.get('/api/v1/tours/:id/', (req, res) => {
+  const id = req.params.id * 1; // convert the string to a number
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body); // without app.use(express.json()) middleware, we get undefined
+
+  const newId = tours[tours.length - 1].id + 1;
+  // Object.assign creates a new object by merging two objects together - won't mutate the original
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((tour) => tour.id === id);
+
+  if (!tour) {
+    return res.status(404).send({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  const updatedTour = { ...tour, ...req.body };
+  const updatedTours = tours.map((tour) =>
+    tour.id === updatedTour.id ? updatedTour : tour
+  );
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(updatedTours),
+    (err) => {
+      res.status(200).send({
+        status: 'success',
+        data: updatedTour,
+      });
+    }
+  );
 });
 
 const port = 3000;
