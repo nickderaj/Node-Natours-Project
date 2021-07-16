377ad70c992b213a04a88fe9a5802099b5e9bd2f
Nick De Raj <nickderaj@gmail.com>
2021-07-16 14:22:06 +0800

    Setting up ESLint with Prettier

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+// Configure everything to do with Express here //
+
 //////////////// EXPRESS ////////////////
+
 const express = require('express');
 const morgan = require('morgan');
 
@@ -8,9 +11,11 @@ const userRouter = require('./routes/userRouter');
 
 //////////////// MIDDLEWARE ////////////////
 const app = express();
-
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
+// app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
