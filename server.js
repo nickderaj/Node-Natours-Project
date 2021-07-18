50299af8f99164fbef4b5451a69357f135362759
Nick De Raj <nickderaj@gmail.com>
2021-07-18 12:24:34 +0800

    Filling out the Tours Schema

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const DB = process.env.DATABASE.replace(
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
   })
