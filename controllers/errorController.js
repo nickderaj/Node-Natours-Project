7c2d6525d955f82167511b1a89560cc416a98542
Nick De Raj <nickderaj@gmail.com>
2021-07-21 14:38:40 +0800

    Error handling during pproduction vs development

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -8,10 +8,23 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error('ERROR 🙄', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
 };
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500; // 500 is internal server error
