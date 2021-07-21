5d4383d2fb98f7e3de7c4ec2640770ef9b0c274a
Nick De Raj <nickderaj@gmail.com>
2021-07-21 13:35:35 +0800

    Adding global error handling

--- app.js
+++ app.js
@@ -26,10 +26,25 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl}`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl}`,
+  // });
+
+  const err = new Error('Test error!');
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500; // 500 is internal server error
+  err.status = err.status || 'error'; // 500 is error, 400 is fail
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
+  next();
 });
 
 module.exports = app;
