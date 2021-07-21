e3e9941de563e64f387b040849d66093b166cf2b
Nick De Raj <nickderaj@gmail.com>
2021-07-21 13:46:40 +0800

    Refactoring error handler into it's own class

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const express = require('express');
 const morgan = require('morgan');
 
 //////////////// FILES ////////////////
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
 
@@ -26,25 +28,9 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl}`,
-  // });
-
-  const err = new Error('Test error!');
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500; // 500 is internal server error
-  err.status = err.status || 'error'; // 500 is error, 400 is fail
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-  next();
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ utils/appError.js
@@ -0,0 +1,12 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true; // use this boolean to only send errors back to the client for operational errors, not programming errors
+
+    Error.captureStackTrace(this, this.constructor); // this error won't pollute the stack trace (err.stack)
+  }
+}
+
+module.exports = AppError;
