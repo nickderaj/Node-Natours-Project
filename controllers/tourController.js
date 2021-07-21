73f575a18f302c6f9d2cfe44e363abf50209de49
Nick De Raj <nickderaj@gmail.com>
2021-07-21 11:24:34 +0800

    Added document middleware w/ slugify module to create slugs

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -105,7 +105,7 @@ exports.getTourStats = async (req, res) => {
   try {
     const stats = await Tour.aggregate([
       {
-        $match: { ratingsAverage: { $gte: 4.5 } },
+        $match: { ratingsAverage: { $gte: 4.5 } }, // search for results >=4.5
       },
       {
         $group: {
@@ -133,6 +133,57 @@ exports.getTourStats = async (req, res) => {
     });
   } catch (err) {
     res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates', // breaks down the array like the ... operator
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStart: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: {
+          _id: 0, // hide the _id value, 1 shows 0 hides
+        },
+      },
+      {
+        $sort: { numTourStart: -1 }, //ascending based on busiest month
+        // $sort: { month: 1 }, // ascending based on month
+      },
+      // {
+      //   $limit: 6,
+      // },
+    ]);
+
+    res.status(200).send({
+      status: 'success',
+      data: plan,
+    });
+  } catch (err) {
+    res.status(404).json({
       status: 'fail',
       message: err,
     });
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,60 +1,87 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true, // removes white space in the end & beginning
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true, // removes white space in the end & beginning
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 0,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a summary'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String, // Store the name of the image in the database, not the actual images
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String], // This is an array of strings
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false, // hides the created at date from public API
+    },
+    startDates: [Date],
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 0,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: {
-    type: Number,
-  },
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a summary'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String, // Store the name of the image in the database, not the actual images
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String], // This is an array of strings
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false, // hides the created at date from public API
-  },
-  startDates: [Date],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE
+// Called before a document is saved to the database - runs before .save() and .create(), NOT for .insertMany()
+tourSchema.pre('save', function (next) {
+  // console.log(this); // 'this' points to the document that is being saved.
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+// QUERY MIDDLEWARE
 
 const Tour = mongoose.model('Tour', tourSchema);
 
