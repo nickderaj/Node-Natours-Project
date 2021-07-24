1b78a2e84012ddeb666fd15798674e14e37f47a3
Nick De Raj <nickderaj@gmail.com>
2021-07-24 18:19:57 +0800

    Adding more packages to protect from attacks

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 //////////////// FILES ////////////////
 const AppError = require('./utils/appError');
@@ -33,6 +36,26 @@ app.use('/api', limiter);
 
 // Body parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitisation against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitsation against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 // Serving static files
 // app.use(express.static(`${__dirname}/public`));
