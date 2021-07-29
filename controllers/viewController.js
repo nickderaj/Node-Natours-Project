c168e56260945d3548979f1fa2f8656ffcc762b1
Nick De Raj <nickderaj@gmail.com>
2021-07-29 21:20:57 +0800

    Added tour routes and filled in the pug templates

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -2,14 +2,15 @@ const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
-  // 1) Get tour data from the collection
   const tours = await Tour.find();
-  // 2) Build template
-
-  // 3) Render that template using tour data
   res.status(200).render('overview', { title: 'The Forest Hiker Tour', tours }); // 'tours: tours' is just tours in ES6
 });
 
-exports.getTour = catchAsync((req, res) => {
-  res.status(200).render('tour', { title: 'All Tours' });
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) Get the data for the requested tour (including guides and reviews)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  res.status(200).render('tour', { title: tour.name, tour });
 });
