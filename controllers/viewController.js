15950f4beac37ae093a64a4602972c775cd8328e
Nick De Raj <nickderaj@gmail.com>
2021-07-29 20:15:12 +0800

    Overview pug template created and filled in

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,7 +1,15 @@
-exports.getTour = (req, res) => {
-  res.status(200).render('overview', { title: 'The Forest Hiker Tour' });
-};
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
 
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', { title: 'All Tours' });
-};
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from the collection
+  const tours = await Tour.find();
+  // 2) Build template
+
+  // 3) Render that template using tour data
+  res.status(200).render('overview', { title: 'The Forest Hiker Tour', tours }); // 'tours: tours' is just tours in ES6
+});
+
+exports.getTour = catchAsync((req, res) => {
+  res.status(200).render('tour', { title: 'All Tours' });
+});
