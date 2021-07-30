f19bdab96773dabc31ee05f1a6db20c7faf3932e
Nick De Raj <nickderaj@gmail.com>
2021-07-30 09:55:53 +0800

    Adding working map to tour pages and placeholder login screen

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -6,11 +6,29 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   res.status(200).render('overview', { title: 'The Forest Hiker Tour', tours }); // 'tours: tours' is just tours in ES6
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the data for the requested tour (including guides and reviews)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user',
   });
-  res.status(200).render('tour', { title: tour.name, tour });
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      'connect-src https://*.tiles.mapbox.com https://api.mapbox.com https://events.mapbox.com'
+    )
+    .render('tour', { title: tour.name, tour });
+});
+
+exports.getLoginForm = catchAsync(async (req, res, next) => {
+  res.status(200).render('login', {
+    title: 'Log into your account',
+  });
+});
+
+exports.getSignupForm = catchAsync(async (req, res, next) => {
+  res.status(200).render('signup', {
+    title: 'Sign Up Today!',
+  });
 });
