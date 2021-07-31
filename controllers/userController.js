6a00e2b410ee2a1b6627ab30defb7566548e2ffb
Nick De Raj <nickderaj@gmail.com>
2021-07-31 11:45:37 +0800

    Added updating user photo functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,53 @@
+const multer = require('multer');
+const sharp = require('sharp');
+
 const User = require('../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
+
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/img/users');
+//   },
+//   filename: (req, file, cb) => {
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//     //user-fdija93r9a03-398a4093843.jpeg
+//   },
+// });
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = (req, res, next) => {
+  if (!req.file) return next();
+
+  // was previously defined by multer upload, so we have to 'redefine' the filename as we use it in the UpdateMe function
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  sharp(req.file.buffer)
+    .resize(500, 500) // 500x500 pixels
+    .toFormat('jpeg') // format to jpeg as file is smaller
+    .jpeg({ quality: 90 }) // quality 90%
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+};
 
 // Factory Methods:
 exports.getAllUsers = factory.getAll(User);
@@ -36,6 +82,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   // 2) Filter the body so they can't alter all details like making themself admin
   const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
   // 3) Update user doc
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -14,7 +14,12 @@ router.use(authController.protect); // protecting all the routes beyond this poi
 
 router.patch('/updatePassword/', authController.updatePassword);
 router.get('/me/', userController.getMe, userController.getUser);
-router.patch('/updateMe/', userController.updateMe);
+router.patch(
+  '/updateMe/',
+  userController.uploadUserPhoto,
+  userController.resizeUserPhoto,
+  userController.updateMe
+); // photo is the field in the form that's going to be uploading the image
 router.delete('/deleteMe/', userController.deleteMe);
 
 router.use(authController.restrictTo('admin')); // only admins beyond this point
